e7439c56efa8d1fa3646d07ee7b7e1ccfa3909e3
linrenyan <linrenyan@yeezon.com>
2019-09-25 16:05:28 +0800

    feat: add element-ui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './icons'
+import ElementUI from 'element-ui'
+import '@/styles/element-variables.scss'
 Vue.config.productionTip = false
 import '@/styles/main.scss'
 
+Vue.use(ElementUI)
 new Vue({
   el: '#app',
   router,
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="index">
     <hello-world msg="Welcome to Your Vue.js App"/>
+    <el-button type="primary">按钮</el-button>
   </div>
 </template>
 <script>
