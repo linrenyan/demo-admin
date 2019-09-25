44cb33986657c9b7d75fe97297c94414f7b891e6
linrenyan <linrenyan@yeezon.com>
2019-09-25 15:39:09 +0800

    feat: add vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import './icons'
 Vue.config.productionTip = false
 import '@/styles/main.scss'
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index">
+    <hello-world msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'index',
+  components: {
+    HelloWorld
+  }
+}
+</script>
